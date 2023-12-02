b0ad2f3d2ddedad064c26324e93e5b773231c9bd
viveksaini97 <151495233+viveksaini97@users.noreply.github.com>
2023-12-01 18:40:26 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import CustomerPage from './CustomerPage';
@@ -11,7 +12,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ShopkeeperPage/>
+   <Router>
+      <LoginForm />
+    </Router>
   </React.StrictMode>
 );
 
